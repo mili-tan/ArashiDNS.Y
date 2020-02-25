62302f59540e614fb90cff909270d84d4a7b9add
Milkey Tan <819774989@qq.com>
2020-02-25 21:43:44 +0800

    添加项目文件。

---
+++ Yuan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ARSoft.Tools.Net;
+using ARSoft.Tools.Net.Dns;
+
+namespace Yuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = new DnsClient(IPAddress.Parse("37.209.192.9"), 5000).Resolve(DomainName.Parse("mili.one"),
+                RecordType.Ns);
+            Console.WriteLine(i.ReturnCode);
+            foreach (var v in i.AuthorityRecords) Console.WriteLine(v.ToString());
+            foreach (var v in i.AnswerRecords) Console.WriteLine(v.ToString());
+            Console.ReadKey();
+        }
+    }
+}
